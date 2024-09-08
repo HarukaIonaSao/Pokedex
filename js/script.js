8bdb23d27df185363cbeccefd4278fb9b5999f7b
HarukaIonaSao <robertaribeiro004@gmail.com>
2024-09-08 12:10:41 -0300

    [feat]:script.js- Adição de atributo de região e geração

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-// Seleciona os elementos HTML
 const pokemonName = document.getElementById('pokemonName');
 const pokemonImage = document.getElementById('pokemonImage');
 const pokemonType = document.getElementById('pokemonType');
+const pokemonRegion = document.getElementById('pokemonRegion'); // Adicione um elemento para exibir a região
+const pokemonGeneration = document.getElementById('pokemonGeneration'); // Adicione um elemento para exibir a geração
 const pokemonIdInput = document.getElementById('pokemonId');
 const fetchPokemonButton = document.getElementById('fetchPokemon');
 
@@ -9,9 +10,7 @@ const fetchPokemonButton = document.getElementById('fetchPokemon');
 function fetchPokemon() {
     const pokemonId = pokemonIdInput.value;
     const apiURL = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-
-    // Define a imagem padrão antes de começar a buscar
-    pokemonImage.src = 'https://encurtador.com.br/WX2ET';
+    const speciesURL = `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`;
 
     fetch(apiURL)
         .then(response => response.json())
@@ -19,11 +18,30 @@ function fetchPokemon() {
             pokemonName.textContent = `Nome: ${data.name}`;
             pokemonImage.src = data.sprites.front_default; // Atualiza com a imagem do Pokémon
             pokemonType.textContent = `Tipo: ${data.types.map(type => type.type.name).join(', ')}`;
+        
+            return fetch(speciesURL);
+        })
+        .then(response => response.json())
+        .then(speciesData => {
+            // Obtém a geração da espécie
+            const generation = speciesData.generation.name;
+            pokemonGeneration.textcontent = `Geração: ${generation}`;
+
+            const generationURL = speciesData.generation.url;
+            return fetch(generationURL);
+        })
+        .then(response => response.json())
+        .then(generationData => {
+
+            const region = generationData.main_region.name;
+            pokemonRegion.textContent = `Região: ${region}`;
         })
         .catch(error => {
             pokemonName.textContent = 'Pokémon não encontrado!';
             pokemonImage.src = 'https://encurtador.com.br/WX2ET'; // Mantém a imagem padrão se não encontrado
             pokemonType.textContent = '';
+            pokemonRegion.textContent = '';
+            pokemonGeneration.textContent = '';
         })
         .finally(() => {
             pokemonIdInput.value = '';
